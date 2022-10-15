4d4eaa0d314f8a62f1d8a6decac6aee0ab21d253
Balconywood <davidcannon789@gmail.com>
2022-10-15 19:42:57 +0100

    Feat: Node zoom, highlight on click

--- src/components/StudentNetwork.tsx
+++ src/components/StudentNetwork.tsx
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback, useRef } from 'react'
 import axios from 'axios';
 import { ForceGraph3D } from 'react-force-graph';
 import SpriteText from 'three-spritetext';
-import { GraphData } from '../types/graphTypes';
+import { ForceGraphMethods, GraphData } from '../types/graphTypes';
 import { Person } from '../types/peopleTypes';
 
 const StudentNetwork = () => {
   const [data, setData] = useState<GraphData>();
+  const graphRef = useRef<ForceGraphMethods>();
 
   const getData = async () => {
     await axios.get<Person[]>("http://localho.st:5000/people").then((resp) => {
@@ -14,9 +15,9 @@ const StudentNetwork = () => {
         axios.get<string[]>("http://localho.st:5000/societies").then((societiesResp) => {
           let mappedData = {
             "nodes": [
-              ...resp.data.map(person => ({"id": person.student_id, "label": person.name, "group": 1})),
-              ...coursesResp.data.map(course => ({"id": course, "group": 2})),
-              ...societiesResp.data.map(society => ({"id": society, "group": 3})),
+              ...resp.data.map(person => ({"id": person.student_id, "label": person.name, "opacity": 1, "group": 1})),
+              ...coursesResp.data.map(course => ({"id": course, "opacity": 0.5, "group": 2})),
+              ...societiesResp.data.map(society => ({"id": society, "opacity": 0.5, "group": 3})),
             ],
             "links": [
               ...resp.data.map(person => ({"source": person.student_id, "target": person.subject}))
@@ -34,6 +35,27 @@ const StudentNetwork = () => {
     });
   }
 
+  const onNodeClick = useCallback((node: any) => {
+    const distance = 100;
+    const distRatio = 1 + distance/Math.hypot(node.x, node.y, node.z);
+
+    graphRef.current?.cameraPosition(
+      { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }, // new position
+      node, // lookAt ({ x, y, z })
+      3000  // ms transition duration
+    );
+
+    let newData = data;
+    const linkedNodeIds = data?.links.filter((link: any) => {
+      return link.source.id === node.id;
+    }).map((link: any) => link.target.id);
+    newData?.nodes.forEach(checkingNode => {
+      if (!linkedNodeIds?.includes(checkingNode.id)) checkingNode.opacity = 0.0;
+      node.opacity = 1.0;
+    });
+    setData(oldData => ({...oldData, ...newData!}));
+  }, [graphRef, data]);
+
   useEffect(() => {
     getData();
   }, []);
@@ -41,14 +63,18 @@ const StudentNetwork = () => {
   return (
     <div>
       <ForceGraph3D
+        ref={graphRef}
         graphData={data}
         nodeAutoColorBy="group"
         nodeThreeObject={(node: any) => {
           const sprite = new SpriteText(node.label ? node.label : node.id);
-          sprite.color = node.color;
+
+          const rgbValues = (node.color as string).substring(1).match(/.{2}/g)?.map(hex => parseInt(hex, 16));
+          sprite.color = `rgba(${rgbValues![0]}, ${rgbValues![1]}, ${rgbValues![2]}, ${node.opacity})`;
           sprite.textHeight = 8;
           return sprite;
         }}
+        onNodeClick={onNodeClick}
       />
     </div>
   )
--- src/types/graphTypes.ts
+++ src/types/graphTypes.ts
@@ -6,10 +6,41 @@ export interface GraphData {
 interface GraphNode {
     id: string,
     label?: string,
+    opacity: number,
     group: number
 }
 
 interface GraphLink {
     source: string,
     target: string
+}
+
+// Copied from react-force-graph index.d.ts
+type Coords = { x: number; y: number; z: number; }
+
+export interface ForceGraphMethods {
+    // Link styling
+  emitParticle(link: any): any;
+
+  // Force engine (d3-force) configuration
+  d3Force(forceName: 'link' | 'charge' | 'center' | string): any | undefined;
+  d3Force(forceName: 'link' | 'charge' | 'center' | string, forceFn: any): any;
+  d3ReheatSimulation(): any;
+
+  // Render control
+  pauseAnimation(): any;
+  resumeAnimation(): any;
+  cameraPosition(position: Partial<Coords>, lookAt?: Coords, transitionMs?: number): any;
+  zoomToFit(durationMs?: number, padding?: number, nodeFilter?: (node: any) => boolean): any;
+  postProcessingComposer(): any;
+  scene(): any;
+  camera(): any;
+  renderer(): any;
+  controls(): object;
+  refresh(): any;
+
+  // Utility
+  getGraphBbox(nodeFilter?: (node: any) => boolean): { x: [number, number], y: [number, number], z: [number, number] };
+  screen2GraphCoords(x: number, y: number, distance: number): Coords;
+  graph2ScreenCoords(x: number, y: number, z: number): Coords;
 }
