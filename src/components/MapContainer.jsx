b0e3e591b52bca5a44b2c42fed69a1509eb7d8d5
ywb20140 <fabian.salge.2020@uni.strath.ac.uk>
2022-10-15 22:04:35 +0100

    Map with svgs on it

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,42 +1,118 @@
-import React, { useState, useRef, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { Map, Marker, GoogleApiWrapper } from "google-maps-react"
 import axios from 'axios';
+import img from '../img.svg'
 
 
 export function MapContainer({ google }) {
 
-  const [data, setData] = useState({});
+  const [locationData, setLocationData] = useState([]);
 
   const getData = async () => {
     await axios.get("http://127.0.0.1:5000/places").then((resp) => {
-        setData(resp.data);
+      setLocationData(resp.data);
     })
   }
 
-useEffect(() => {
+  useEffect(() => {
     getData();
-}, [])
+  }, [])
+
   
+  const mapStyle = [
+    {
+      "elementType": "labels",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.land_parcel",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.neighborhood",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "labels.text",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.business",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "transit",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    }
+  ]
+
+  function _mapLoaded(mapProps, map) {
+    map.setOptions({
+       styles: mapStyle
+    })
+ }
 
   return (
     <div className='w-full h-screen overflow-hidden'>
-      <div className='w-full h-[200px]'>
-          <button onClick={() => console.log(data)}>TEstttt</button>
-      </div>
       <Map
         google={google}
         style={{ width: '100%', height: '100%', position: 'relative' }}
+        onReady={(mapProps, map) => _mapLoaded(mapProps, map)}
         className={'map'}
+        initialCenter={{ lat: 55.871843, lng: -4.288341 }}
+        zoom={17}
+      clickableIcons={false}
+      disableDefaultUI={true}
 
-        zoom={14}
-
-        disableDefaultUI={true}
       >
-        
-        <Marker onClick={() => console.log('test')}
-          name={'Current location'}
-          position={{ lat: 55.8728276, lng: -4.2844698}} />
-      </Map>
+      {locationData.map((place, index) => (
+        <Marker onClick={() => alert(place.name)}
+          key={index}
+          name={place.name}
+          position={{ lat: place.location.latitude, lng: place.location.longitude }}
+          icon={{
+            url: img,
+            anchor: new google.maps.Point(32, 32),
+            scaledSize: new google.maps.Size(50, 50)
+          }}
+        />
+
+      ))}
+    </Map>
     </div >
   )
 };
