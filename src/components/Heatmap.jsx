6696445cce4fbdbdc3aa95790d6649e7e9ed58dd
ywb20140 <fabian.salge.2020@uni.strath.ac.uk>
2022-10-15 18:33:30 +0100

    Theres a mapppp

---
+++ src/components/Heatmap.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useRef, useEffect } from 'react'
+import CustomMap from './CustomMap'
+
+
+const Heatmap = () => {
+
+  
+
+  return (
+    <div style={{ width: 200, height: 200 }}>
+      <CustomMap  />
+    </div>
+  )
+}
+
+export default Heatmap
